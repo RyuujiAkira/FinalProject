cc7e1d79706c72fb01efe3a45a150a8bc700d98f
Fraser Smith <fraser.n.smith@gmail.com.com>
2021-12-05 13:36:02 +0000

    database code initialised

---
+++ BackEnd/routes/bookings.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+
+const { MongoBookings } = require('../persistence/MongoBookings.js');
+
+router.post('/create', (req, res, next) => {
+
+
+    const booking = new MongoBookings(req.body);
+    console.log(req.body);
+    console.log(booking);
+
+
+    booking.save()
+
+    .then((result) => {
+        res.status(201).send(`${result} saved to database!`);
+    })
+
+    .catch((error) => {
+
+        next(error);
+    });
+
+});
+
+router.get('/getAll', (req, res) => {
+
+
+    lizard.find((error, bookingList) => {
+        if (error) {
+            console.log(`error :( : ${error}`);
+        }
+        res.status(200).send(bookingList);
+    })
+})
+
+module.exports = router;
