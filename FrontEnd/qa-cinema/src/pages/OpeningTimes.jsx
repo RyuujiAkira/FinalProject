6a6a4560b0e02496629ac76b57ca6f6558cdedcd
RyuujiAkira <jrabajac@gmail.com>
2021-12-06 17:13:13 +0000

    Created Opening Times page
    Created an opening times page with CSS to change background colour and added a list of all the days and times needed.

---
+++ FrontEnd/qa-cinema/src/pages/OpeningTimes.jsx
@@ -0,0 +1,49 @@
+const OpeningTimes = () => {
+
+    return (
+        <div>
+            <div class="px-4">
+                <div class="overflow-hidden">
+                    <div class="container px-5">
+                        <img src="https://queensmere.shoppertainmentmanagement.co.uk/wp-content/uploads/2018/02/open_text.png" class="img-fluid" alt="Cinema screen" width="1100" height="900" loading="lazy" />
+                    </div>
+                </div>
+            </div>
+            <div class="container px-4">
+                <div class="row gx-5">
+                    <div class="col">
+                        <div class="p-3 border bg-light">
+                            <div class="card">
+                                <div class="card-header">
+                                    Opening Hours & Days
+                                </div>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item text-center py-4">Monday : 10:00 - 22:00</li>
+                                    <li class="list-group-item text-center py-4">Tuesday : 09:00 - 22:00</li>
+                                    <li class="list-group-item text-center py-4">Wednesday : 09:00 - 22:00</li>
+                                    <li class="list-group-item text-center py-4">Thursday : 09:00 - 23:00</li>
+                                    <li class="list-group-item text-center py-4">Friday : 09:00 - 22:00</li>
+                                    <li class="list-group-item text-center py-4">Saturday : 10:00 - 23:00</li>
+                                    <li class="list-group-item text-center py-4">Sunday : 11:00 - 20:00</li>
+                                    <br />
+                                    <li class="list-group-item text-center py-4">
+                                        <h5>Closed on Seasonal Holidays
+                                            <br />and Bank Holidays
+                                        </h5>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-auto">
+                        <div class="p-3 border bg-light">
+                            <img src="https://images.unsplash.com/photo-1524985069026-dd778a71c7b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80" alt="Couple At Cinema" class="img-fluid" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default OpeningTimes;
