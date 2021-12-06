daaca117d20b40fac20028f63cd1744dbc7bdf1c
Kieran0507 <k.goodinson94@gmail.com>
2021-12-06 14:22:02 +0000

    QACF-20 #comment Basic navigation and routes completed

--- FrontEnd/qa-cinema/src/App.js
+++ FrontEnd/qa-cinema/src/App.js
@@ -1,12 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import Mongo from "../../../BackEnd/persistence/MongoBookings";
 
 import NavBar from "./pages/components/NavBar";
 function App() {
   return (
     <div className="App">
       <NavBar />
-
     </div>
   );
 }
