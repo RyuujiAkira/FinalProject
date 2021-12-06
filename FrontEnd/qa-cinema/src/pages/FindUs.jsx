8623bc51b24b1f131ee7929146532a13eb980d08
Fraser Smith <fraser.n.smith@gmail.com.com>
2021-12-06 17:23:32 +0000

    QACF-59 #comment mapping done

---
+++ FrontEnd/qa-cinema/src/pages/FindUs.jsx
@@ -0,0 +1,10 @@
+import BingMap from "./components/BingMap";
+
+const FindUs = () => {
+    return (<div>
+        <h1>WERE HERE!!!</h1>
+        <BingMap />
+    </div>);
+}
+
+export default FindUs;
