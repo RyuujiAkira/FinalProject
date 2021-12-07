801d67f645fcf6e8170dc2b7439919e3192bbe54
Fraser Smith <fraser.n.smith@gmail.com.com>
2021-12-07 14:45:56 +0000

    added movie functionality

--- BackEnd/routes/bookings.js
+++ BackEnd/routes/bookings.js
@@ -24,12 +24,42 @@ router.post('/create', (req, res, next) => {
 
 });
 
+router.post('/direct', (req, res, next) => {
 
+
+    const movie = new Movies(req.body);
+    console.log(req.body);
+    console.log(movie);
+
+
+    movie.save()
+
+    .then((result) => {
+        res.status(201).send(`${result} saved to database!`);
+    })
+
+    .catch((error) => {
+
+        next(error);
+    });
+
+});
 
 router.get('/getAll', (req, res) => {
 
     console.log("getAll");
     MongoBookings.find((error, bookingList) => {
+        if (error) {
+            console.log(`error :( : ${error}`);
+        }
+        res.send(bookingList).status(202);
+    })
+})
+
+router.get('/watchAll', (req, res) => {
+
+    console.log("watchAll");
+    Movies.find((error, bookingList) => {
         if (error) {
             console.log(`error :( : ${error}`);
         }
