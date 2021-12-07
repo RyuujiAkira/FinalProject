975fd79a5680152c042015f261b8fc6d1a8dcb4f
Fraser Smith <fraser.n.smith@gmail.com.com>
2021-12-07 15:18:48 +0000

    fixing merge conflicts

---
+++ FrontEnd/qa-cinema/src/pages/newListings.jsx
@@ -0,0 +1,38 @@
+const Listings = () => {
+
+  //   Have api calls in here?
+  return <> <h5>Below is the listings gallery. All the current movies out now!</h5>
+  <div class="row">
+<div class="px-4">
+  <img width="300px" height="450px" src="https://c4.wallpaperflare.com/wallpaper/764/590/391/inception-leonardo-dicaprio-movie-posters-2400x3500-entertainment-movies-hd-art-wallpaper-preview.jpg" ></img>
+ <p>Inception (2010)
+  Leonardo DiCaprio </p> <p>Joseph Gordon-Levitt, Ellen Page</p>
+  <p>Directed by: Christopher Nolan </p>
+  </div>
+  <div class="px-3">
+  <img width="300px" height="450px" src="https://lh3.googleusercontent.com/proxy/eGfQdANPba-zKNVQ7TPa5YYRBNxP213cIlgRVB4020dZu9j0uzICYtKhK7sHf2vGTjmEq68YMxayrK_ehuyqs_Wwu-1BSxeaMBXFoYWJRxwsv8IpqqO8"></img>
+  <p>Avengers Endgame (2019)
+  Robert Downey Jr. </p> <p>Chris Hemsworth, Chris Evans</p>
+  <p>Directed by: Anthony Russo, Joe Russo </p>
+  </div>
+  <div class="px-3">
+  <img width="300px" height="450px" src="https://w0.peakpx.com/wallpaper/467/882/HD-wallpaper-black-widow-movie-poster-marvel-cinematic-universe-portrait-display-movies-women-scarlett-johansson.jpg"></img>
+  <p>Black Widow (2021)
+  Scarlett Johansson </p> <p>Florence Pugh, David Harbour</p>
+  <p>Directed by: Cate Shortland </p>
+  </div>
+  <div class="px-3">
+  <img width="300px" height="450px" src="https://gdj-inr5u0ip5pewom.stackpathdns.com/wp-content/uploads/2012/05/movie-poster-20.jpg"></img>
+  <p>Battleship (2012)
+Taylor Kitsch </p> <p>Peter Berg, Rihanna</p>
+  <p>Directed by: Peter Berg </p>
+</div>
+</div>
+
+{/* pagination!!!!! */}
+
+
+</>
+};
+
+export default Listings;
