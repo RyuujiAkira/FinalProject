180326423e5218d9c629c04a65cc9765cc33b110
Fraser Smith <fraser.n.smith@gmail.com.com>
2021-12-07 16:56:04 +0000

    added form for bookings

--- FrontEnd/qa-cinema/src/pages/Bookings.jsx
+++ FrontEnd/qa-cinema/src/pages/Bookings.jsx
@@ -1,5 +1,25 @@
 const Bookings = () => {
-  return <div></div>;
+  return (<div>
+    <h1>Make your Booking</h1>
+    <span>Movie</span>
+
+    <input type="text" />
+    <span>Date</span>
+    <input type="datetime-local" />
+    <span>Username</span>
+    <input type="text" />
+    <span>Seats</span>
+    <input type="number" />
+    <span>Adult</span>
+    <input type="number" />
+    <span>Child</span>
+    <input type="number" />
+    <span>Concession</span>
+    <input type="number" />
+
+    <p>Is your order booked?</p>
+    <button type="button">Book Now</button>
+  </div>);
 };
 
 export default Bookings;
