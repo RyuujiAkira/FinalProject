053fe1dec0aa382ad525b42ca85373b50cb02fa6
Adam-Ateye <adamateye1994@gmail.com>
2021-12-07 17:40:13 +0000

    base structure for discussion page

---
+++ FrontEnd/qa-cinema/src/pages/Discussion.jsx
@@ -0,0 +1,90 @@
+const Discussion = () => {
+    return (
+        <div className="Discussion">
+            <div className="container">
+                <h1>Discussion</h1>
+                <div className="row">
+                <div className="row">
+                    <p className="text-center">Most recent reviews</p>
+                    <div className="col-md-4 text-center">
+                    <div className="profile">
+                        <img src="user.png" className="user" />
+                        <blockquote>Review1</blockquote>
+                        <h3>
+                        Movie1 <span> username</span>
+                        </h3>
+                    </div>
+                    </div>
+                    <div className="col-md-4 text-center">
+                    <div className="profile">
+                        <img src="user.png" className="user" />
+                        <blockquote>Review2</blockquote>
+                        <h3>
+                        Movie2 <span> username</span>
+                        </h3>
+                    </div>
+                    </div>
+                    <div className="col-md-4 text-center">
+                    <div className="profile">
+                        <img src="user.png" className="user" />
+                        <blockquote>Review3</blockquote>
+                        <h3>
+                        Movie3 <span> username</span>
+                        </h3>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div className="forum">
+                <ol>
+                <li className="row1">
+                    <a className="btn" href="/Thread.html">
+                    <h5 className="title">Movie4</h5>
+                    <div className="bottom">
+                        <p className="timestamp">Date</p>
+                        <p className="comment-count">Number of comments</p>
+                    </div>
+                    </a>
+                </li>
+                <li className="row1">
+                    <a className="btn" href="/Thread.html">
+                    <h5 className="title">Movie5</h5>
+                    <div className="bottom">
+                        <p className="timestamp">Date</p>
+                        <p className="comment-count">Number of comments</p>
+                    </div>
+                    </a>
+                </li>
+                <li className="row1">
+                    <a className="btn" href="/Thread.html">
+                    <h5 className="title">Movie6</h5>
+                    <div className="bottom">
+                        <p className="timestamp">Date</p>
+                        <p className="comment-count">Number of comments</p>
+                    </div>
+                    </a>
+                </li>
+                <li className="row1">
+                    <a className="btn" href="/Thread.html">
+                    <h5 className="title">Movie7</h5>
+                    <div className="bottom">
+                        <p className="timestamp">Date</p>
+                        <p className="comment-count">Number of comments</p>
+                    </div>
+                    </a>
+                </li>
+                <li className="row1">
+                    <a className="btn" href="/Thread.html">
+                    <h5 className="title">Movie8</h5>
+                    <div className="bottom">
+                        <p className="timestamp">Date</p>
+                        <p className="comment-count">Number of comments</p>
+                    </div>
+                    </a>
+                </li>
+                </ol>
+            </div>
+        </div>
+    )
+}
