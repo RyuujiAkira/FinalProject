162f831c50c1d05e13966a00f2a048579c4b6652
sarahkgh <sarahk.work@outlook.com>
2021-12-13 17:01:46 +0000

    QACF-65 #updates to footer

--- FrontEnd/qa-cinema/src/pages/components/Footer.jsx
+++ FrontEnd/qa-cinema/src/pages/components/Footer.jsx
@@ -1,46 +1,45 @@
-
-import React from 'react';
-
+import React from "react";
 
 const Footer = () => {
-    return ( <div className="main-footer">
-        <div className="container">
-            <div className="row">
-                <h4>QA Cinema Inc</h4>
-                <ul className="list-unstyled">
-                    <li>8, St James's Bldg</li>
-                    <li>61-95 Oxford St,</li>
-                    <li>Manchester M1 6FQ</li>
+  return (
+    <div class="main-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <h4>QA Cinema Inc</h4>
 
-                </ul>
-                </div>
-                <div className="column2">
-
-                <div>
-                <div className="col">
-                <a href="http://localhost:3000/">Home Page</a>
-                </div>
-            </div>
-
-            <div className="col">
-            <a href="http://localhost:3000/about">About Us</a>
-            </div>
-
-            <div className="col">
-        <a href="http://localhost:3000/contactus">Contact Us</a> 
-            </div>
-            </div>
-            <hr />
-            <div className="row">
-                <p className="col-sm">
-                    &copy;{new Date().getFullYear()} QA CINEMA INC | All right reserved | Terms Of Service | Privacy
-                </p>
-            </div>
+            <br></br>
+            <ul class="list-unstyled">
+              <li>8, St James's Bldg</li>
+              <li>61-95 Oxford St,</li>
+              <li>Manchester M1 6FQ</li>
+            </ul>
+          
         </div>
-         {/* <a href="http://localhost:3000/">Home Page</a>
+
+        <div class="col">
+         <ul>
+           <li><a href="http://localhost:3000/">Home Page</a></li> 
+           <li> <a href="http://localhost:3000/about">About Us</a></li>
+           <li> <a href="http://localhost:3000/contactus">Contact Us</a></li>
+          </ul>
+          </div>
+        </div>
+
+        
+        <hr />
+        <div class="row">
+          <p class="col-sm">
+            &copy;{new Date().getFullYear()} QA CINEMA INC | All right reserved
+            | Terms Of Service | Privacy
+          </p>
+        </div>
+      </div>
+      {/* <a href="http://localhost:3000/">Home Page</a>
         <a href="http://localhost:3000/about">About Us</a>
         <a href="http://localhost:3000/contactus">Contact Us</a>  */}
-        </div> );
-}
- 
+    </div>
+  );
+};
+
 export default Footer;
